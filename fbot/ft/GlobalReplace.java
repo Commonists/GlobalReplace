d1462331e8e9bca1496fb17443bf3dcfe3e72f17
Marco <falke.marco@gmail.com>
2015-06-27 15:31:31 +0200

    Some minor refactoring and GUI cleanup

--- fbot/ft/GlobalReplace.java
+++ fbot/ft/GlobalReplace.java
@@ -23,9 +23,9 @@ import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 public class GlobalReplace {
-    private static W commonsWiki;
+    private static W wiki;
     private static final String SIGN_UP = "Commons:GlobalReplace/Sign-in";
-    private static final String VERSION = "v0.3.2";
+    private static final String VERSION = "v0.3.3";
     private static final String TITLE = "GlobalReplace " + VERSION;
     private static final JTextField OLD_TF;
     private static final JTextField NEW_TF;
@@ -44,7 +44,7 @@ public class GlobalReplace {
     }
 
     public static void main(String[] args) {
-        commonsWiki = FGUI.login();
+        wiki = FGUI.login();
         GlobalReplace.signup();
         SwingUtilities.invokeLater(new Runnable(){
 
@@ -57,11 +57,11 @@ public class GlobalReplace {
 
     private static void createAndShowGUI() {
     	// Settings
-    	OLD_TF.setToolTipText("Hint: Use Ctrl+v or Command+v to paste text");
-        NEW_TF.setToolTipText("Hint: Use Ctrl+v or Command+v to paste text");
-        REASON_TF.setToolTipText("Hint: Enter an optional edit summary");
+    	OLD_TF.setToolTipText("Use Ctrl+v or Command+v to paste text");
+        NEW_TF.setToolTipText("Use Ctrl+v or Command+v to paste text");
+        REASON_TF.setToolTipText("Enter an optional edit summary");
         BAR.setStringPainted(true);
-        BAR.setString(String.format("Hello, %s! :)", commonsWiki.whoami()));
+        BAR.setString(String.format("Hello, %s! :)", wiki.whoami()));
         BUTTON.addActionListener(new ActionListener(){
 
             @Override
@@ -72,7 +72,7 @@ public class GlobalReplace {
 
         // Create GUI
         JFrame f = FGUI.simpleJFrame(TITLE, 3, true);
-        f.getContentPane().add((Component)FGUI.buildForm(TITLE, new JLabel("Old Title: "), OLD_TF, new JLabel("New Title: "), NEW_TF, new JLabel("Summary: "), REASON_TF), "Center");
+        f.getContentPane().add((Component)FGUI.buildForm("Replacement", new JLabel("Old Title: "), OLD_TF, new JLabel("New Title: "), NEW_TF, new JLabel("Summary: "), REASON_TF), "Center");
         f.getContentPane().add((Component)FGUI.boxLayout(1, FGUI.simpleJPanel(BUTTON), BAR), "South");
         FGUI.setJFrameVisible(f);
     }
@@ -81,12 +81,12 @@ public class GlobalReplace {
      * Sign up any new user of this tool; Do nothing if the user already signed up
      */
     private static void signup() {
-        String text = commonsWiki.getPageText(SIGN_UP);
+        String text = wiki.getPageText(SIGN_UP);
         if (text == null) {
             return;
         }
-        if (!text.contains(commonsWiki.whoami())) {
-            boolean success = commonsWiki.edit(SIGN_UP, String.valueOf(text.trim()) + "\n#~~~~", "Signing up via "
+        if (!text.contains(wiki.whoami())) {
+            boolean success = wiki.edit(SIGN_UP, text.trim() + "\n#~~~~", "Signing up via "
             		+ TITLE);
             if (!success) {
                 JOptionPane.showConfirmDialog(null, "You are not allowed to use this tool; Please request permission at "
@@ -111,7 +111,7 @@ public class GlobalReplace {
         private GRThread() {
             this.old_name = Namespace.nss(OLD_TF.getText()).trim();
             this.new_name = Namespace.nss(NEW_TF.getText()).trim();
-            this.reason = String.valueOf(REASON_TF.getText().trim().replace((CharSequence)"%s", (CharSequence)"%%s")) + " ([[%sCommons:GlobalReplace|%s]])";
+            this.reason = REASON_TF.getText().trim().replace("%s", "%%s") + " ([[%sCommons:GlobalReplace|%s]])";
             this.makeRegex();
         }
 
@@ -124,7 +124,7 @@ public class GlobalReplace {
                 BUTTON.setText("Stop");
                 GlobalReplace.negateActivated();
                 this.doJob();
-                commonsWiki.switchDomain("commons.wikimedia.org");
+                wiki.switchDomain("commons.wikimedia.org");
                 BUTTON.setText("Start");
             } else {
                 BUTTON.setEnabled(false);
@@ -136,35 +136,42 @@ public class GlobalReplace {
             BAR.setValue(0);
             BUTTON.setEnabled(false);
             this.setTextFieldState(false);
-            ArrayList<Tuple<String, String>> l = commonsWiki.globalUsage("File:" + this.old_name);
+            ArrayList<Tuple<String, String>> list = wiki.globalUsage("File:" + this.old_name);
             BUTTON.setEnabled(true);
-            if (l == null || l.size() == 0) {
+            if (list == null || list.size() == 0) {
                 BAR.setString(String.format("'%s' is not globally used", this.old_name));
             } else {
-                BAR.setMaximum(l.size());
+                BAR.setMaximum(list.size());
                 String domain = null;
                 String text = null;
-                for (int i = 0; i < l.size(); ++i) {
-                    if (!this.updateStatus(i, l.get(i))) {
+                for (int i = 0; i < list.size(); ++i) {
+                    if (!this.updateStatus(i, list.get(i))) {
                         return;
                     }
-                    if (domain != l.get((int)i).y) {
-                        domain = (String)l.get((int)i).y;
-                        commonsWiki.switchDomain(domain);
+                    if (domain != list.get(i).y) {
+                        domain = list.get(i).y;
+                        wiki.switchDomain(domain);
                     }
-                    if ((text = commonsWiki.getPageText((String)l.get((int)i).x)) == null) continue;
+                    if ((text = wiki.getPageText(list.get(i).x)) == null) continue;
                     Object[] arrobject = new Object[2];
-                    arrobject[0] = domain.contains((CharSequence)"commons") ? "" : "Commons:";
+                    arrobject[0] = domain.contains("commons") ? "" : "Commons:";
                     arrobject[1] = TITLE;
-                    commonsWiki.edit((String)l.get((int)i).x, text.replaceAll(this.regex, this.new_name), String.format(this.reason, arrobject));
+                    wiki.edit(list.get(i).x, text.replaceAll(this.regex, this.new_name), String.format(this.reason, arrobject));
                 }
+                BAR.setValue(list.size());
                 BAR.setString("Done!");
             }
             this.setTextFieldState(true);
             GlobalReplace.negateActivated();
         }
 
-        private boolean updateStatus(int i, Tuple<String, String> t) {
+        /**
+         * Check if the program is still activated and update the progress bar
+         * @param progress how much work was done already
+         * @param tuple the string tuple with ...
+         * @return if the program is activated
+         */
+        private boolean updateStatus(int progress, Tuple<String, String> tuple) {
             if (!activated) {
                 BAR.setValue(0);
                 BAR.setString("Interrupted by user");
@@ -172,8 +179,8 @@ public class GlobalReplace {
                 BUTTON.setEnabled(true);
                 return false;
             }
-            BAR.setValue(i + 1);
-            BAR.setString(String.format("Edit %s @ %s (%d/%d)", t.x, t.y, i + 1, BAR.getMaximum()));
+            BAR.setValue(progress);
+            BAR.setString(String.format("Edit %s @ %s (%d/%d)", tuple.x, tuple.y, progress + 1, BAR.getMaximum()));
             return true;
         }
 
@@ -191,9 +198,12 @@ public class GlobalReplace {
             this.regex = this.regex.replaceAll("( |_)", "( |_)");
         }
 
+        /**
+         * Check if old and new name are valid; Notify user if they are not 
+         * @return if the names are valid
+         */
         private boolean sanityCheck() {
-            boolean status;
-            boolean bl = status = WikiFile.canUpload(this.old_name) && WikiFile.canUpload(this.new_name);
+            boolean status = WikiFile.canUpload(this.old_name) && WikiFile.canUpload(this.new_name);
             if (!status) {
                 JOptionPane.showMessageDialog(null, "You can only replace valid file names");
             }
